590e882f450d1d869f92a7f8d9a0966f279759d3
sinanmsc <sinanmsc13@gmail.com>
2024-01-01 16:18:46 +0530

    text_style and space extension
    created extensions for space and text_style

--- lib/core/theme/theme_helper.dart
+++ lib/core/theme/theme_helper.dart
@@ -1,4 +1,6 @@
 import 'package:delicious/core/theme/extensions/colors_extension.dart';
+import 'package:delicious/core/theme/extensions/space_extension.dart';
+import 'package:delicious/core/theme/extensions/typography_extension.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
@@ -8,5 +10,13 @@ class AppTheme {
 
   AppColors get colors {
     return Theme.of(context).extension<AppColors>()!;
+  }
+
+  AppSpaceExtension get spaces {
+    return Theme.of(context).extension<AppSpaceExtension>()!;
+  }
+
+  AppTypographyExtension get typography {
+    return Theme.of(context).extension<AppTypographyExtension>()!;
   }
 }
